21059407ade9e0f9be5c75009ca886c11a193fd4
kritisharma1202 <kri.sha1202@gmail.com>
2023-04-21 18:43:43 -0500

    setup frontend structure for stock lookup, build stock lookup form, display stock price and stock objects in browser, dealing with invalid search, use ajax and javascript, many to many association, stock listing view, adding restrictions, removing stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -6,6 +10,14 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex_client[:secret_token_key],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    return client.quote(ticker_symbol).latest_price
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
